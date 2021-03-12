cf519dd8e199ed69cd9d77818a0474cf4fc90438
Mario <mariogarcia2506@gmail.com>
2021-03-12 01:25:27 +0100

    mod

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def get_predictions(y_real, y_pred):
+    '''Returns a dataframe with data passed
+    Inputs: arrays
+    Returns: DataFrame
+    '''
     try:
         df = pd.DataFrame(y_real)
         df["prediction"] = y_pred
@@ -9,13 +13,17 @@ def get_predictions(y_real, y_pred):
         print(error)
         print(f"The parameters must have the same length")
 
-def plot_importances(model, data):
-    sorted_importances = model.feature_importances_.argsort()
-    
-    plt.figure(figsize=(15,15))
-    
-    return plt.barh(data.columns[sorted_importances], model.feature_importances_[sorted_importances])
 
-def get_merge(data, y_pred):
-    return pd.merge(data, pd.DataFrame(y_pred, columns=["y_pred"]), left_index=True, right_index=True)
+
+def plot_importances(model, data):
+    '''Plots sorted importances about the features
+    Input: Model and Data
+    Returns: Plot with the sorted importance of each feature.
+    '''
+    #Gets the features importances and sorts them
+    sorted_importances = model.feature_importances_.argsort()
+    #Sets the figure size
+    plt.figure(figsize=(15,15))
+    #Returns the plot
+    return plt.barh(data.columns[sorted_importances], model.feature_importances_[sorted_importances])
   
